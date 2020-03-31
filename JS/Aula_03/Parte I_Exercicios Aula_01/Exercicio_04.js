1c68f32a089bc6f86c1bc5da1eda125180e52dc5
Ana Higo <anahigo.arq@gmail.com>
2020-03-30 22:52:40 -0300

    Exercise 01

---
+++ JS/Aula_03/Parte I_Exercicios Aula_01/Exercicio_04.js
@@ -0,0 +1,19 @@
+/* 4- Faça um programa que, dado um valor definido numa variável, retorne “positive” se esse valor for positivo, “negative” se for negativo e “zero” caso contrário.  
+
+------Interpretando o enunciado------
+- number
+- se number for maior que 0 imprima positive 
+- se number for menor que 0 imprima negative
+- se number for igual a 0 imprima zero
+*/
+
+function number(n) {
+    if (n > 0) {
+        return 'positive';
+    } else if (n < 0) {
+        return 'negative';
+    } else {
+        return 'zero';
+    }
+}    
+console.log(number(0))
---
+++ JS/Aula_03/Parte I_Exercicios Aula_01/Exercicio_07.js
@@ -0,0 +1,47 @@
+/* 7- Escreva um programa que converte uma nota dada em porcentagem (de 0 a 100) em conceitos de A a F. Siga essas regras:
+
+Porcentagem >= 90 -> A
+
+Porcentagem >= 80 -> B
+
+Porcentagem >= 70 -> C
+
+Porcentagem >= 60 -> D
+
+Porcentagem >= 50 -> E
+
+Porcentagem < 50 -> F
+
+O programa deve retornar uma mensagem de erro e encerrar se a nota passada for menor que 0 ou maior que 100. 
+
+------Interpretando o enunciado------
+
+- nota - 0 a 100
+- se a nota for maior ou igual a 90 e menor e igual a 100 - imprima nota A 
+- se a nota for maior ou igual a 80 e menor que 90 - imprima nota B
+- se a nota for maior ou igual a 70 e menor que 80 - imprima nota C
+- se a nota for maior ou igual a 60 e menor que 70 - imprima nota D
+- se a nota for maior ou igual a 50 e menor que 60 - imprima nota E
+- se a nota for maio ou igual a 0 e menor  que 50 - imprima nota F   
+- se a nota for menor que 0 ou maior que 100 - imprima nota inválida
+*/
+
+
+function result(note) {
+    if (note >= 90 && note <= 100) {
+        return 'Note A'
+    } else if (note >= 80 && note < 90) {
+        return 'Note B'
+    } else if (note >= 70 && note < 80) {
+        return 'Note C'
+    } else if (note >= 60 && note < 70) {
+        return 'Note D'
+    } else if (note >= 50 && note < 60) {
+        return 'Note E'
+    } else if (note >= 0 && note < 50) {
+        return 'Note F'
+    } else (note < 0 && note > 100) 
+        return 'Invalid Note'
+}    
+
+console.log(result(95))
